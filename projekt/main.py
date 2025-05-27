cbdb159da8fba936789445f506a9004f3390cd6c
khan2999 <n.ahmadzai86@yahoo.com>
2025-05-27 14:41:42 +0200

    mainbugfixed

--- projekt/main.py
+++ projekt/main.py
@@ -1,25 +1,26 @@
-# main.py – Einstiegspunkt für das Chat-Programm
-
+# main.py
 import multiprocessing
 from ui import run_ui
 from discovery import run_discovery_service
 from network import run_network_service
 from config import load_config
 
+
 def main():
     config = load_config()
 
-    # Pipes für die Kommunikation zwischen UI und den Services
+    # Pipes
     ui_to_net, net_from_ui = multiprocessing.Pipe()
     ui_to_disc, disc_from_ui = multiprocessing.Pipe()
     net_to_ui, ui_from_net = multiprocessing.Pipe()
     disc_to_ui, ui_from_disc = multiprocessing.Pipe()
 
-    # Discovery- und Netzwerk-Prozesse starten
+    # Discovery und Network als Prozesse
     discovery_process = multiprocessing.Process(
         target=run_discovery_service,
         args=(disc_from_ui, disc_to_ui, config)
     )
+
     network_process = multiprocessing.Process(
         target=run_network_service,
         args=(net_from_ui, net_to_ui, config)
@@ -28,13 +29,12 @@ def main():
     discovery_process.start()
     network_process.start()
 
-    try:
-        # UI im Hauptprozess laufen lassen (für funktionierendes input() sonst beendet sich der Prozess)
-        run_ui(ui_to_net, ui_from_net, ui_to_disc, ui_from_disc, config)
-    finally:
-        # Beim Beenden UI die anderen Prozesse beenden
-        discovery_process.terminate()
-        network_process.terminate()
+    # UI im Hauptprozess
+    run_ui(ui_to_net, ui_from_net, ui_to_disc, ui_from_disc, config)
+
+    # Nach Beenden der UI:
+    network_process.terminate()
+    discovery_process.terminate()
 
 
 if __name__ == '__main__':
